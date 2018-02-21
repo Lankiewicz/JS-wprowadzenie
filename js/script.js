1fe4511e23fe03f9685494462b655dfe00d754b3
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-02-21 21:29:56 +0100

    add test coment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 <script>
 var a = 5,
 b = 6;
-//przypisanie
+//przypisanie zmiennej
 triangleArea = a * h / 2
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
